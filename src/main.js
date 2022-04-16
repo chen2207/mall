420e8ab217d219605d5ca364297d3b53cffe16f1
qinyu chen <2207143608@qq.com>
2022-04-16 09:46:27 +0800

    商品分类基本完成

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import './plugins/element.js'
 import 'element-ui/lib/theme-chalk/index.css'
 import './assets/fonts/iconfont.css'
 import axios from 'axios'
+import TreeTable from 'vue-table-with-tree-grid'
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 axios.interceptors.request.use(config => {
   config.headers.Authorization = window.sessionStorage.getItem('token')
@@ -13,6 +14,7 @@ axios.interceptors.request.use(config => {
 })
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
+Vue.component('tree-table', TreeTable)
 
 new Vue({
   render: h => h(App),
--- src/router.js
+++ src/router.js
@@ -6,6 +6,7 @@ import Welcome from './components/welcome.vue'
 import Users from './components/user/users.vue'
 import Rights from './components/power/Rights.vue'
 import Roles from './components/power/roles.vue'
+import Cate from './components/goods/cate.vue'
 Vue.use(Router)
 
 const router = new Router({
@@ -19,6 +20,7 @@ const router = new Router({
         { path: '/users', component: Users },
         { path: '/rights', component: Rights },
         { path: '/roles', component: Roles },
+        { path: '/categories', component: Cate },
       ]
     },
   ],
